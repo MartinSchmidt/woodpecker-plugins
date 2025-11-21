d7569b674214fb4dd5c3d0d928c199c434d23d2b
Martin Schmidt Daugaard <msd@mjolner.dk>
2025-11-21 09:37:53 +0100

    Remove CI_STEP_NAME check and environment variable setup from Docker-in-Docker entrypoint script

--- plugins/docker-in-docker/plugin-entrypoint.sh
+++ plugins/docker-in-docker/plugin-entrypoint.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
 
-if [ -z "$CI_STEP_NAME" ]; then
-    echo "Error: CI_STEP_NAME is not set"
-    exit 1
-fi
+set -e
 
 FOLDER=/woodpecker/${PLUGIN_FOLDER:-dind}
 mkdir -p "${FOLDER}"
-
-
 export DOCKER_TLS_CERTDIR=${FOLDER}/certs
-
-cat <<EOF > "${FOLDER}/.env"
-export DOCKER_HOST='tcp://${CI_STEP_NAME}:2376'
-export DOCKER_CERT_PATH="${DOCKER_TLS_CERTDIR}/client"
-export DOCKER_TLS_VERIFY=1
-EOF
 
 exec "$@"
